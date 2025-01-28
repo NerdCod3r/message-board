eb617e46030abf4d3abc176490def6ba845c15c7
root <root@A>
2025-01-28 23:23:43 +0200

    I have done all routes, they're not tested against the checker. I have to do functional tests using chai.

--- routes/api.js
+++ routes/api.js
@@ -153,4 +153,146 @@ module.exports = function (app) {
     }
   });
 
+  /**
+   * This method is used to delete an entire thread from a certain board
+   * thread_id and delete_password will be passed using GET.
+   */
+  app.route('/api/threads/:board')
+  .delete(async function(req, res){
+    const { board } = req.params;
+    const { thread_id, delete_password } = req.body;
+
+    console.log('thread_id ', thread_id);
+    console.log('delete_password ', delete_password);
+   
+    if (!thread_id || !delete_password) {
+      res.status(400).send('Missing required fields');
+    }
+
+    try {
+      console.log('Inside try-catch block');
+      const thread = await Thread.findOne({ _id: thread_id, board });
+      console.log(thread);
+      if (!thread) {
+       res.send('Thread not found');
+      }
+  
+      if (thread.delete_password !== delete_password) {
+       res.send('Incorrect password');
+      }
+
+      if (thread.delete_password === delete_password) {
+        await thread.deleteOne();
+        res.send('success');
+      }
+
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Unable to delete thread');
+    }
+
+  });
+  
+  // DELETE a reply 
+  app.route('/api/replies/:board')
+  .delete(async function(req, res){
+
+    const { board } = req.params;
+    const { thread_id, reply_id, delete_password } = req.body;
+
+    if (!thread_id || !reply_id || !delete_password) {
+      res.send('Missing required fields');
+    }
+
+    try {
+      const thread = await Thread.findOne({ _id: thread_id, board });
+  
+      if (!thread) {
+        res.send('Thread not found');
+      }
+  
+      const reply = thread.replies.id(reply_id);
+  
+      if (!reply) {
+        res.send('Reply not found');
+      }
+  
+      if (reply.delete_password !== delete_password) {
+        res.send('Incorrect delete password');
+      }
+  
+      // Replace reply text with '[deleted]'
+      reply.text = '[deleted]';
+      await thread.save();
+  
+      res.send('Reply successfully deleted');
+    } catch (err) {
+      console.error(err);
+      res.send('Unable to delete reply');
+    }
+  });
+
+  // REPORT a Thread : PUT(/api/threads/:board)
+  app.route('/api/threads/:board')
+  .put(async function(req, res){
+
+    const { board } = req.params;
+    const { thread_id } = req.body;
+
+    if (!thread_id) {
+      res.send('Missing required thread_id');
+    }
+
+    try {
+      const thread = await Thread.findOneAndUpdate(
+        { _id: thread_id, board },
+        { reported: true },
+        { new: true }
+      );
+  
+      if (!thread) {
+        res.send('Thread not found');
+      }
+  
+      res.send('Thread successfully reported');
+    } catch (err) {
+      console.error(err);
+      res.send('Unable to report thread');
+    }
+  });
+
+  // REPORT a reply: PUT('/api/replies/:board)
+  app.route('/api/replies/:board')
+  .put(async function(req, res){
+
+    const { board } = req.params;
+    const { thread_id, reply_id } = req.body;
+
+    if (!thread_id || !reply_id) {
+      res.send('Missing required fields');
+    }
+
+    try {
+      const thread = await Thread.findOne({ _id: thread_id, board });
+  
+      if (!thread) {
+        res.send('Thread not found');
+      }
+  
+      const reply = thread.replies.id(reply_id);
+  
+      if (!reply) {
+       res.send('Reply not found');
+      }
+  
+      reply.reported = true;
+      await thread.save();
+  
+      res.send('Reply successfully reported');
+    } catch (err) {
+      console.error(err);
+      res.send('Unable to report reply');
+    }
+  
+  });
 };
